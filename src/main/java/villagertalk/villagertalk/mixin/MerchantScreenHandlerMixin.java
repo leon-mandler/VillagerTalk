deabee02792c4ba085fc687c4df7b3b7df44000d
leonm <leon.mandler@gmx.at>
2024-03-27 23:42:51 +0100

    Implemented Mixins to detect VillagerInventory opening.

---
+++ src/main/java/villagertalk/villagertalk/mixin/MerchantScreenHandlerMixin.java
@@ -0,0 +1,17 @@
+package villagertalk.villagertalk.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.MerchantScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import villagertalk.villagertalk.VillagerTalk;
+
+@Mixin(MerchantScreenHandler.class)
+public abstract class MerchantScreenHandlerMixin {
+    @Inject(at = @At("HEAD"), method = "onClosed(Lnet/minecraft/entity/player/PlayerEntity;)V")
+    private void closeHandledScreen(PlayerEntity player, CallbackInfo info) {
+        VillagerTalk.onVillagerTradeClose(player);
+    }
+}
---
+++ src/main/java/villagertalk/villagertalk/mixin/VillagerEntityMixin.java
@@ -0,0 +1,17 @@
+package villagertalk.villagertalk.mixin;
+
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import villagertalk.villagertalk.VillagerTalk;
+
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin{
+    @Inject(at = @At("HEAD"), method = "beginTradeWith(Lnet/minecraft/entity/player/PlayerEntity;)V")
+    private void beginTradeWith(PlayerEntity customer, CallbackInfo info) {
+        VillagerTalk.onVillagerTradeOpen(customer,  (VillagerEntity)(Object)this);
+    }
+}

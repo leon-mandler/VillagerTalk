ba7c052a5981a8fb82627c4b306d164237b3e8b9
leonm <leon.mandler@gmx.at>
2024-06-20 00:27:57 +0200

    Added JavaDoc and removed commented out methods.

--- src/client/java/villagertalk/villagertalk/VillagerTalkClient.java
+++ src/client/java/villagertalk/villagertalk/VillagerTalkClient.java
@@ -1,7 +1,6 @@
 package villagertalk.villagertalk;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.util.Identifier;
 public class VillagerTalkClient implements ClientModInitializer {
 
 	@Override
